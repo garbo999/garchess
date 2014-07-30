6b3f5529e1aa4262af4776f6607aaf8fb39fb425
Gary Hess <garyhess999@gmail.com>
2014-07-30 05:45:26 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Garchess::Application.config.secret_token = '0ac9d7d15a405faceb75304010a8fb6a8c401b2fa695ddc2519352177e6a53e06a854015c5d737e6b04c8225efe3b29bfab7b8dca06ba4afe4b9c2e082fe6681'
